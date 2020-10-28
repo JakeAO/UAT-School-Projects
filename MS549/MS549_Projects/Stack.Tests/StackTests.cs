defa022c2bb7615ae8992426d0a6d081ad09fed1
Jake O'Connor <jake.a.oconnor@gmail.com>
2020-10-28 10:31:34 -0700

    MS549 Assignment 1 - Full Test Suite and Implementation
    * Implemented Stack<T>.
    * Added more tests to the suite.

--- MS549/MS549_Projects/Stack.Tests/StackTests.cs
+++ MS549/MS549_Projects/Stack.Tests/StackTests.cs
@@ -50,6 +50,121 @@ namespace SadPumpkin.Stack.Tests
             Assert.AreEqual(0, newStack.Peek());
         }
 
+        [TestCase(new int[0], ExpectedResult = 0)]
+        [TestCase(new[] {1}, ExpectedResult = 1)]
+        [TestCase(new[] {1, 1}, ExpectedResult = 2)]
+        [TestCase(new[] {1, 1, 1}, ExpectedResult = 3)]
+        [TestCase(new[] {1, 1, 1, 1}, ExpectedResult = 4)]
+        [TestCase(new[] {1, 1, 1, 1, 1}, ExpectedResult = 5)]
+        public int count_returns_accurate_count(params int[] values)
+        {
+            IStack<int> newStack = new Stack<int>(values.Length);
+            
+            foreach (int value in values)
+            {
+                newStack.Push(value);
+            }
+
+            return newStack.Count;
+        }
+
+        [TestCase(new int[0])]
+        [TestCase(new[] {1})]
+        [TestCase(new[] {1, 1})]
+        [TestCase(new[] {1, 1, 1})]
+        [TestCase(new[] {1, 1, 1, 1})]
+        [TestCase(new[] {1, 1, 1, 1, 1})]
+        public void count_is_accurate_as_elements_are_added_and_removed(params int[] values)
+        {
+            IStack<int> newStack = new Stack<int>(values.Length);
+
+            Assert.AreEqual(0, newStack.Count);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                newStack.Push(values[i]);
+                Assert.AreEqual(i + 1, newStack.Count);
+            }
+
+            Assert.AreEqual(values.Length, newStack.Count);
+
+            for (int i = values.Length; i > 0; i--)
+            {
+                newStack.Pop();
+                Assert.AreEqual(i - 1, newStack.Count);
+            }
+
+            Assert.AreEqual(0, newStack.Count);
+        }
+
+        [TestCase(new int[0])]
+        [TestCase(new[] {1})]
+        [TestCase(new[] {1, 1})]
+        [TestCase(new[] {1, 1, 1})]
+        [TestCase(new[] {1, 1, 1, 1})]
+        [TestCase(new[] {1, 1, 1, 1, 1})]
+        public void capacity_remains_same_when_elements_fit(params int[] values)
+        {
+            IStack<int> newStack = new Stack<int>(values.Length);
+            
+            Assert.AreEqual(values.Length, newStack.Capacity);
+            
+            foreach (int value in values)
+            {
+                newStack.Push(value);
+            }
+            
+            Assert.AreEqual(values.Length, newStack.Capacity);
+        }
+
+        [TestCase(0, new[] {1}, ExpectedResult = 1)]
+        [TestCase(1, new[] {1, 1}, ExpectedResult = 2)]
+        [TestCase(2, new[] {1, 1, 1}, ExpectedResult = 4)]
+        [TestCase(3, new[] {1, 1, 1, 1}, ExpectedResult = 6)]
+        [TestCase(1, new[] {1, 1, 1, 1, 1}, ExpectedResult = 8)]
+        public int capacity_doubles_when_elements_do_not_fit(int capacity, params int[] values)
+        {
+            IStack<int> newStack = new Stack<int>(capacity);
+
+            Assert.AreEqual(capacity, newStack.Capacity);
+
+            foreach (int value in values)
+            {
+                newStack.Push(value);
+            }
+
+            Assert.AreEqual(values.Length, newStack.Count);
+
+            return newStack.Capacity;
+        }
+
+        [TestCase(0, new[] {1}, ExpectedResult = 1)]
+        [TestCase(1, new[] {1, 1}, ExpectedResult = 2)]
+        [TestCase(2, new[] {1, 1, 1}, ExpectedResult = 4)]
+        [TestCase(3, new[] {1, 1, 1, 1}, ExpectedResult = 6)]
+        [TestCase(1, new[] {1, 1, 1, 1, 1}, ExpectedResult = 8)]
+        public int capacity_remains_fixed_when_elements_are_removed(int capacity, params int[] values)
+        {
+            IStack<int> newStack = new Stack<int>(capacity);
+
+            Assert.AreEqual(capacity, newStack.Capacity);
+
+            foreach (int value in values)
+            {
+                newStack.Push(value);
+            }
+
+            Assert.AreEqual(values.Length, newStack.Count);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                newStack.Pop();
+            }
+            
+            return newStack.Capacity;
+        }
+
+        [TestCase(new int[0], ExpectedResult = 0)]
         [TestCase(new[] {1}, ExpectedResult = 1)]
         [TestCase(new[] {1, 2}, ExpectedResult = 2)]
         [TestCase(new[] {1, 2, 3}, ExpectedResult = 3)]
@@ -58,6 +173,24 @@ namespace SadPumpkin.Stack.Tests
         public int peek_returns_top_value(params int[] values)
         {
             IStack<int> newStack = new Stack<int>(values.Length);
+            
+            foreach (int value in values)
+            {
+                newStack.Push(value);
+            }
+
+            return newStack.Peek();
+        }
+
+        [TestCase(new[] {1}, ExpectedResult = 1)]
+        [TestCase(new[] {1, 2}, ExpectedResult = 2)]
+        [TestCase(new[] {1, 2, 3}, ExpectedResult = 3)]
+        [TestCase(new[] {1, 2, 3, 4}, ExpectedResult = 4)]
+        [TestCase(new[] {1, 2, 3, 4, 5}, ExpectedResult = 5)]
+        public int push_inserts_new_value(params int[] values)
+        {
+            IStack<int> newStack = new Stack<int>(values.Length);
+            
             foreach (int value in values)
             {
                 newStack.Push(value);
@@ -74,6 +207,7 @@ namespace SadPumpkin.Stack.Tests
         public int pop_returns_top_value(params int[] values)
         {
             IStack<int> newStack = new Stack<int>(values.Length);
+            
             foreach (int value in values)
             {
                 newStack.Push(value);
@@ -90,6 +224,7 @@ namespace SadPumpkin.Stack.Tests
         public int pop_removes_top_value(params int[] values)
         {
             IStack<int> newStack = new Stack<int>(values.Length);
+            
             foreach (int value in values)
             {
                 newStack.Push(value);
@@ -97,6 +232,27 @@ namespace SadPumpkin.Stack.Tests
 
             newStack.Pop();
             return newStack.Peek();
+        }
+
+        [TestCase(new[] {1, 2, 3})]
+        [TestCase(new[] {1, 2, 3, 4})]
+        [TestCase(new[] {1, 2, 3, 4, 5})]
+        [TestCase(new[] {1, 2, 3, 4, 5, 6})]
+        public void order_is_maintained(params int[] values)
+        {
+            IStack<int> newStack = new Stack<int>(values.Length);
+
+            foreach (int value in values)
+            {
+                newStack.Push(value);
+            }
+
+            for (int i = values.Length - 1; i >= 0; i--)
+            {
+                int val = newStack.Pop();
+
+                Assert.AreEqual(values[i], val);
+            }
         }
     }
 }
--- MS549/MS549_Projects/Stack/Stack.cs
+++ MS549/MS549_Projects/Stack/Stack.cs
@@ -1,35 +1,46 @@
-﻿namespace SadPumpkin.Stack
+﻿using System;
+
+namespace SadPumpkin.Stack
 {
     public class Stack<T> : IStack<T>
     {
         private T[] _array;
         private int _topIdx;
-        private int _count;
 
         public Stack(int capacity = 0)
         {
             _array = new T[capacity];
-            _topIdx = 0;
-            _count = 0;
+            _topIdx = -1;
         }
 
-        public int Count => _count;
+        public int Count => _topIdx + 1;
 
         public int Capacity => _array.Length;
 
         public void Push(T value)
         {
-            throw new System.NotImplementedException();
+            if (_array.Length == 0)
+                Array.Resize(ref _array, 1);
+            while (_topIdx >= _array.Length - 1)
+                Array.Resize(ref _array, _array.Length * 2);
+
+            _array[++_topIdx] = value;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if (_topIdx == -1)
+                throw new InvalidOperationException("Attempted Pop() when Stack had no elements.");
+
+            return _array[_topIdx--];
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (_topIdx == -1)
+                return default;
+
+            return _array[_topIdx];
         }
     }
 }
